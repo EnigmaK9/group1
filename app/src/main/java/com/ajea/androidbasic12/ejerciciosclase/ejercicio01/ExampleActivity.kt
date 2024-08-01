510a5f7d16e218212fe41d934a5c5e3449775d89
Alan11774 <alanblacksky@gmail.com>
2024-07-31 21:55:52 -0600

    entrega1 entregas de primer actividad

--- app/src/main/java/com/ajea/androidbasic12/ejerciciosclase/ejercicio01/ExampleActivity.kt
+++ app/src/main/java/com/ajea/androidbasic12/ejerciciosclase/ejercicio01/ExampleActivity.kt
@@ -1,16 +1,17 @@
-package com.ajea.androidbasic12
+package com.ajea.androidbasic12.ejerciciosclase.ejercicio01
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.ajea.androidbasic12.R
 
-class MainActivity : AppCompatActivity() {
+class ExampleActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_example)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
