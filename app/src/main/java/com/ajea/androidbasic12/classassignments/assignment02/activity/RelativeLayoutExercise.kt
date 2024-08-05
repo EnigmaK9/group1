56c50ecc43f9f83e3fd22640cd416434127c9e50
Alan11774 <alanblacksky@gmail.com>
2024-08-05 17:49:44 -0600

    linearlayout Activity for person viewed in class , modified import to match with the correct project files

---
+++ app/src/main/java/com/ajea/androidbasic12/classassignments/assignment02/activity/RelativeLayoutExercise.kt
@@ -0,0 +1,21 @@
+package com.ajea.androidbasic12.classassignments.assignment02.activity
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.ajea.androidbasic12.R
+
+class RelativeLayoutExercise : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_relative_layout_exercise)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
---
+++ app/src/main/java/com/ajea/androidbasic12/classassignments/assignment02/personslist/PersonEntity.kt
@@ -0,0 +1,7 @@
+package com.ajea.androidbasic12.classassignments.assignment02.personslist
+
+data class PersonEntity(
+    val name: String,
+    val lastName: String,
+    val photo: String
+)
