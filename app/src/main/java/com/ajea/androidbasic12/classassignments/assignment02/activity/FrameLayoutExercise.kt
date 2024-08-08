90fdff1b6deb193b6412cd1ab3f0002052362da6
seb's <sebs@sebss-MacBook-Pro.local>
2024-08-07 18:38:09 -0600

    Framelayout Actividad 4

--- app/src/main/java/com/ajea/androidbasic12/classassignments/assignment02/activity/FrameLayoutExercise.kt
+++ app/src/main/java/com/ajea/androidbasic12/classassignments/assignment02/activity/FrameLayoutExercise.kt
@@ -1,21 +1,14 @@
 package com.ajea.androidbasic12.classassignments.assignment02.activity
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.ajea.androidbasic12.R
 
 class FrameLayoutExercise : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_frame_layout_exercise)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        // Aquí puedes agregar cualquier lógica adicional que necesites para manejar la vista
     }
 }
